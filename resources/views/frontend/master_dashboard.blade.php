8d7327f1aa562b735f2f70461525d807c4aa144b
pocari1210 <pocari.design@gmail.com>
2023-07-28 00:11:11 +0900

    ajax通信雛形作成

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -6,6 +6,9 @@
   <title>Shop Site Online Store </title>
   <meta http-equiv="x-ua-compatible" content="ie=edge" />
   <meta name="description" content="" />
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta property="og:title" content="" />
   <meta property="og:type" content="" />
@@ -68,6 +71,17 @@
   <!-- Template  JS -->
   <script src="{{ asset('frontend/assets/js/main.js?v=5.3') }}"></script>
   <script src="{{ asset('frontend/assets/js/shop.js?v=5.3') }}"></script>
+
+  <script type="text/javascript">
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('centent')
+      }
+    })
+
+    /// Start product view with Modal 
+  </script>
+
 </body>
 
 </html>
