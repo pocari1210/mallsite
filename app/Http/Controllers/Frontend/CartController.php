eabf2f1de6ab57ab832c37d766890c16bbf41435
pocari1210 <pocari.design@gmail.com>
2023-08-05 04:22:25 +0900

    小計の計算処理

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -132,6 +132,19 @@ class CartController extends Controller
   public function CartRemove($rowId)
   {
     Cart::remove($rowId);
+
+    if (Session::has('coupon')) {
+      $coupon_name = Session::get('coupon')['coupon_name'];
+      $coupon = Coupon::where('coupon_name', $coupon_name)->first();
+
+      Session::put('coupon', [
+        'coupon_name' => $coupon->coupon_name,
+        'coupon_discount' => $coupon->coupon_discount,
+        'discount_amount' => round(Cart::total() * $coupon->coupon_discount / 100),
+        'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount / 100)
+      ]);
+    }
+
     return response()->json(['success' => 'Successfully Remove From Cart']);
   } // End Method
 
@@ -139,6 +152,18 @@ class CartController extends Controller
   {
     $row = Cart::get($rowId);
     Cart::update($rowId, $row->qty + 1);
+
+    if (Session::has('coupon')) {
+      $coupon_name = Session::get('coupon')['coupon_name'];
+      $coupon = Coupon::where('coupon_name', $coupon_name)->first();
+
+      Session::put('coupon', [
+        'coupon_name' => $coupon->coupon_name,
+        'coupon_discount' => $coupon->coupon_discount,
+        'discount_amount' => round(Cart::total() * $coupon->coupon_discount / 100),
+        'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount / 100)
+      ]);
+    }
 
     return response()->json('Increment');
   } // End Method
