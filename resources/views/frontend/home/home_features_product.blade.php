2aa69b5f3d3061c14c8f018221ffab2625ff781a
pocari1210 <pocari.design@gmail.com>
2023-08-14 14:13:19 +0900

    Frontendページにレビュー疎通

--- resources/views/frontend/home/home_features_product.blade.php
+++ resources/views/frontend/home/home_features_product.blade.php
@@ -58,41 +58,59 @@ $featured = App\Models\Product::where('featured',1)
                       <a href="shop-grid-right.html">{{ $product['category']['category_name'] }}</a>
                     </div>
                     <h2><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}">{{ $product->product_name }}</a></h2>
+
+                    @php
+                    $reviewcount = App\Models\Review::where('product_id',$product->id)->where('status',1)->latest()->get();
+                    $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1)->avg('rating');
+                    @endphp
+
                     <div class="product-rate d-inline-block">
-                      <div class="product-rating" style="width: 80%"></div>
-                    </div>
+                      @if($avarage == 0)
 
-                    @if($product->discount_price == NULL)
-                    <div class="product-price mt-10">
-                      <span>${{ $product->selling_price }} </span>
+                      @elseif($avarage == 1 || $avarage < 2) <div class="product-rating" style="width: 20%">
                     </div>
-                    @else
-                    <div class="product-price mt-10">
-                      <span>${{ $product->discount_price }} </span>
-                      <span class="old-price">${{ $product->selling_price }}</span>
-                    </div>
-                    @endif
-
-                    <div class="sold mt-15 mb-15">
-                      <div class="progress mb-5">
-                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                    </div>
-                    <a href="shop-cart.html" class="btn w-100 hover-up"><i class="fi-rs-shopping-cart mr-5"></i>Add To Cart</a>
+                    @elseif($avarage == 2 || $avarage < 3) <div class="product-rating" style="width: 40%">
                   </div>
+                  @elseif($avarage == 3 || $avarage < 4) <div class="product-rating" style="width: 60%">
                 </div>
-                <!--End product Wrap-->
-                @endforeach
-
+                @elseif($avarage == 4 || $avarage < 5) <div class="product-rating" style="width: 80%">
               </div>
+              @elseif($avarage == 5 || $avarage < 5) <div class="product-rating" style="width: 100%">
+            </div>
+            @endif
+          </div>
+
+          @if($product->discount_price == NULL)
+          <div class="product-price mt-10">
+            <span>${{ $product->selling_price }} </span>
+          </div>
+          @else
+          <div class="product-price mt-10">
+            <span>${{ $product->discount_price }} </span>
+            <span class="old-price">${{ $product->selling_price }}</span>
+          </div>
+          @endif
+
+          <div class="sold mt-15 mb-15">
+            <div class="progress mb-5">
+              <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
           </div>
-          <!--End tab-pane-->
-
+          <a href="shop-cart.html" class="btn w-100 hover-up"><i class="fi-rs-shopping-cart mr-5"></i>Add To Cart</a>
         </div>
-        <!--End tab-content-->
       </div>
-      <!--End Col-lg-9-->
+      <!--End product Wrap-->
+      @endforeach
+
     </div>
+  </div>
+  </div>
+  <!--End tab-pane-->
+
+  </div>
+  <!--End tab-content-->
+  </div>
+  <!--End Col-lg-9-->
+  </div>
   </div>
 </section>
