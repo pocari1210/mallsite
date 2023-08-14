c4b91b7c18c1459afd2032297694862cab315790
pocari1210 <pocari.design@gmail.com>
2023-08-14 13:47:08 +0900

    reviewページのテーブルレイアウト変更

--- resources/views/vendor/backend/review/approve_review.blade.php
+++ resources/views/vendor/backend/review/approve_review.blade.php
@@ -30,9 +30,10 @@
           <thead>
             <tr>
               <th>Sl</th>
-              <th>Comment </th>
-              <th>User </th>
+              <th>Image </th>
               <th>Product </th>
+              <th>User </th>
+              <th>Comment </th>
               <th>Rating </th>
               <th>Status </th>
             </tr>
@@ -41,9 +42,10 @@
             @foreach($review as $key => $item)
             <tr>
               <td> {{ $key+1 }} </td>
-              <td>{{ Str::limit($item->comment, 25);  }}</td>
-              <td>{{ $item['user']['name'] }}</td>
+              <td> <img src="{{ asset($item['product']['product_thambnail']) }}" style="width: 40px; height:40px;"></td>
               <td>{{ $item['product']['product_name'] }}</td>
+              <td>{{ $item['user']['name'] }}</td>
+              <td>{{ Str::limit($item->comment, 25);  }}</td>
               <td>
                 @if($item->rating == NULL)
                 <i class="bx bxs-star text-secondary"></i>
@@ -99,9 +101,10 @@
           <tfoot>
             <tr>
               <th>Sl</th>
-              <th>Comment </th>
-              <th>User </th>
+              <th>Image </th>
               <th>Product </th>
+              <th>User </th>
+              <th>Comment </th>
               <th>Rating </th>
               <th>Status </th>
             </tr>
